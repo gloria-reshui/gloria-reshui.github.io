90b1eccc833ebd15307a30549aab28f000dd67f9
热水_reshui <karonirasen@gmail.com>
2025-03-01 14:59:22 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import { easyConfig } from './src/utils/easyConfig';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/',
+	base: easyConfig.githubPagesUrl,
 	vite: {
 		resolve: {
 			preserveSymlinks: true, // Allows symlinking of public/personal-blog/posts to src/content/blog
